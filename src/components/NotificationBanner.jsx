771100c455ac4fdd1b5ab64fb96b030802c0a761
ghostjaby assil <altf4irl@gmail.com>
2024-06-06 13:16:10 +0300

    Submission of Exercise 7.20.-7.21.

--- src/components/NotificationBanner.jsx
+++ src/components/NotificationBanner.jsx
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux"
+import { Alert } from "react-bootstrap"
 
 const NotificationBanner = () => {
     const notification = useSelector((state) => state.notification)
 
-    return <div className={notification.class}>{notification.message}</div>
+    return <div className='container' ><Alert variant={notification.class}>{notification.message}</Alert></div>
 }
 
 export default NotificationBanner
